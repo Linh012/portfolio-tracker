7aab99aa6ffc9064237a8325462c593d75fcc6aa
Linh012 <gln8984@gmail.com>
2020-04-11 02:46:51 +0200

    Create chart generation module

---
+++ app/charts.py
@@ -0,0 +1,18 @@
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models import HoverTool
+
+tools = "pan,box_zoom,wheel_zoom,save,reset"
+
+def create_pchart(x,y):
+        # create a new plot with a title and axis labels
+    p = figure(tools=tools,title="Price Chart", x_axis_type="datetime",
+               x_axis_label='Datetime', y_axis_label='Price')
+
+    # add a line renderer with legend and line thickness
+    p.line(x, y, legend="Price", line_width=2)
+    hover = HoverTool()
+    hover.tooltips = "<div style=padding=5px>Price:@y</div>"
+    p.add_tools(hover)
+
+    return p
