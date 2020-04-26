36a062db0d3ec4d9b89095f9aeb2b195a0a964c4
Linh012 <gln8984@gmail.com>
2020-04-26 17:51:19 +0200

    Add comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,20 @@
-from flask import Flask  #external libraries/other imports
+from flask import Flask  #micro web application library
 from flask_sqlalchemy import SQLAlchemy #Object-relational mapper (ORM)
-from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user #User/login sessions
-from config import *
+from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user #User/Login sessions
+from config import * #import configurations
 
-# Initialization/Instantiation of extensions/...
-# creates an instance of Flask class
-app = Flask(__name__, template_folder="templates", static_folder="static")
+app = Flask(__name__, template_folder="templates", static_folder="static") #creates an instance of Flask class
 app.config.from_object('config.DevelopmentConfig')  #environment (development/production/...)
-db = SQLAlchemy(app)  # Database
+db = SQLAlchemy(app)  #Database
 
-# User session management
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
-login_manager.login_message = "You need to be signed in."
-login_manager.login_message_category = "info"
+# User/Login session management
+login_manager = LoginManager() #Initialization
+login_manager.init_app(app) #Instantiation
+login_manager.login_view = "login" #Redirect to login page if not logged in
+login_manager.login_message = "You need to be signed in." #Message if not logged in
+login_manager.login_message_category = "info" #Type of message
 
-from app import routes, models  # routes.py, models.py
+#To avoid circular import error/circular dependency, import code was put here
+from app import routes, models
 
-db.create_all()  # Database + tables creation
+db.create_all()  #Database and tables creation
