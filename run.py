589fd22bd4e4e88df6bba0644328a5498f2c7373
Linh012 <gln8984@gmail.com>
2020-04-26 17:54:13 +0200

    Add comments and change host

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-from app import app  # from app package (app folder with __init__.py)
+from app import app  #import app package (app folder with __init__.py)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=5000)  # runs app
-
-    # debug True = automatically updates web app when code is edited
-    # host='0.0.0.0'
-    # port=5000
+    app.run(host='0.0.0.0', port=5000)
+    #run flask app
